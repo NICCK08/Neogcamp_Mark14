92fbd4a818aa03859747481d332c33347f217293
NICCK08 <65999730+NICCK08@users.noreply.github.com>
2022-10-02 18:46:23 +0530

    Updated js

--- app.js
+++ app.js
@@ -3,9 +3,7 @@ var stocksQuantity = document.querySelector("#stocks-quantity");
 var currentPrice = document.querySelector("#current-price");
 var submitBtn = document.querySelector("#submit-btn");
 var outputBox = document.querySelector("#output-box");
-
 submitBtn.addEventListener("click", submitHandler);
-
 function submitHandler() {
   var ip = Number(initialPrice.value);
   var qty = Number(stocksQuantity.value);
@@ -13,19 +11,16 @@ function submitHandler() {
 
   calculateProfitAndLoss(ip, qty, curr);
 }
-
 function calculateProfitAndLoss(initial, quantity, current) {
   if (initial > current) {
     var loss = (initial - current) * quantity;
     var lossPercentage = (loss / initial) * 100;
-
     showOutput(
       `Hey, the loss is ${loss} and the percent is ${lossPercentage}% 😥`
     );
   } else if (current > initial) {
     var profit = (current - initial) * quantity;
     var profitPercentage = (profit / initial) * 100;
-
     showOutput(
       `Hey, the profit is ${profit} and the percent is ${profitPercentage}% 😁`
     );
@@ -33,7 +28,6 @@ function calculateProfitAndLoss(initial, quantity, current) {
     showOutput(`No pain no gain and no gain no pain💪`);
   }
 }
-
 function showOutput(message) {
   outputBox.innerHTML = message;
 }
